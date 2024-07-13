85e4258e8c6372cf60b54de144eab768406ce7eb
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2024-07-13 13:33:50 +0700

    separate api, dockerise apis and frontend, create a reverse proxy

---
+++ udagram-api-users/src/config/config.ts
@@ -0,0 +1,17 @@
+export const config = {
+  'username': process.env.POSTGRES_USERNAME,
+  'password': process.env.POSTGRES_PASSWORD,
+  'database': process.env.POSTGRES_DB,
+  'host': process.env.POSTGRES_HOST,
+  'dialect': 'postgres',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': process.env.URL,
+  'ssl': {
+    required: true,
+  },
+  'jwt': {
+    'secret': process.env.JWT_SECRET,
+  },
+};
