8f77382bc0d4a3958d1f2824bd17a8a4d60ce202
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2024-07-13 20:45:43 +0700

    deployment config files, evidence screenshots, and adjust api url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a150a4295cc7046afbcbc3aea0713c9a-1520438844.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
